e34205fef851e6e73f17f456072c9b1c40ba2d3b
Agustin Palmieri <agustinpfs@gmail.com>
2020-05-05 12:34:34 -0300

    curso page. accordion OK

--- js/page.js
+++ js/page.js
@@ -5,23 +5,23 @@ let j;
 console.log(acordeonB.length);
 
 for (j = 0; j < acordeonB.length; j++) {
-  acordeonB[j].onclick = function() {
-    this.classList.toggle("activado");
-    let contenido = this.nextElementSibling;
-    console.log(this.nextElementSibling);
-    
-    if (contenido.style.maxHeight){
-      // console.log(" hay max height");
-      // console.log(maxHeight + " max");
-      
-      contenido.style.maxHeight = null;
-    } else {
-      // console.log(" no hay max height");
-      contenido.style.maxHeight = contenido.scrollHeight + "px";
-    } 
-  }
+    acordeonB[j].onclick = function() {
+        this.classList.toggle("activado");
+        let contenido = this.nextElementSibling;
+        console.log(this.nextElementSibling);
+        
+        if (contenido.style.maxHeight){
+            // console.log(" hay max height");
+            // console.log(maxHeight + " max");
+            
+            contenido.style.maxHeight = null;
+        } else {
+            // console.log(" no hay max height");
+            contenido.style.maxHeight = contenido.scrollHeight + "px";
+        } 
+    }
 }
+document.getElementById("desplegado").click();
 //scrollHeight es una medida de la altura del contenido de un elemento,
 // incluido el contenido no visible en la pantalla debido al desbordamiento
-document.getElementById("desplegado").click();
 console.log("hola");
