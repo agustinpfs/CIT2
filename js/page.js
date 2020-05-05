6346d813f91fa7cd67ed7aa0cc192339492a426e
Agustin Palmieri <agustinpfs@gmail.com>
2020-05-05 10:03:39 -0300

    curso page. accordion

--- js/page.js
+++ js/page.js
@@ -1,12 +1,12 @@
 // ACCORDION PAGE 
 
-let acordeonB = document.getElementsByClassName("Acordeon-page");
+let acordeonB = document.getElementsByClassName("Programa__button");
 let j;
 console.log(acordeonB.length);
 
 for (j = 0; j < acordeonB.length; j++) {
   acordeonB[j].onclick = function() {
-    this.classList.toggle("act");
+    this.classList.toggle("activado");
     let contenido = this.nextElementSibling;
     console.log(this.nextElementSibling);
     
@@ -23,5 +23,5 @@ for (j = 0; j < acordeonB.length; j++) {
 }
 //scrollHeight es una medida de la altura del contenido de un elemento,
 // incluido el contenido no visible en la pantalla debido al desbordamiento
-document.getElementById("activ").click();
+document.getElementById("desplegado").click();
 console.log("hola");
